a5e9de060a14468894eb07551dcebeb068499331
Tyler-Parr <tyler@logisol.tech>
2026-03-13 22:22:02 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import StreamingResponse
+from fastapi.middleware.cors import CORSMiddleware
 from rembg import remove
 from io import BytesIO
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/remove-bg")
 async def remove_bg(file: UploadFile = File(...)):
     input_bytes = await file.read()
-
     output_bytes = remove(input_bytes)
 
     return StreamingResponse(
